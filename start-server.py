7efbc55228baa9f62d9cd56c3a7ac23a1ebc206a
senjinpc-25 <hattaryoga@Macbook-Air.local>
2025-07-24 09:54:51 +0900

    start-server.pyを追加 - package.jsonで参照されているファイル

---
+++ start-server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+
+PORT = 8000
+DIRECTORY = "public"
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+    print(f"Server running at http://localhost:{PORT}/")
+    httpd.serve_forever()
