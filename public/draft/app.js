85e44579bfea08859e00509f6e6502a86e86c391
senjinpc-25 <hattaryoga@Macbook-Air.local>
2025-07-31 08:46:55 +0900

    draftディレクトリの画像パスを相対パスに修正
    - /images/から../images/に変更
    - draftディレクトリから親ディレクトリのimagesフォルダを参照

--- public/draft/app.js
+++ public/draft/app.js
@@ -2247,13 +2247,13 @@ class RankingApp {
                     <div class="ribbon_point_box_no">
                         <div class="case-slick-slider" data-slider-id="${rank}">
                             <div class="case-slide">
-                                <img src="/images/clinics/dio/dio_case/dio_case01.jpg" alt="症例1" loading="lazy">
+                                <img src="../images/clinics/dio/dio_case/dio_case01.jpg" alt="症例1" loading="lazy">
                             </div>
                             <div class="case-slide">
-                                <img src="/images/clinics/dio/dio_case/dio_case02.jpg" alt="症例2" loading="lazy">
+                                <img src="../images/clinics/dio/dio_case/dio_case02.jpg" alt="症例2" loading="lazy">
                             </div>
                             <div class="case-slide">
-                                <img src="/images/clinics/dio/dio_case/dio_case03.jpg" alt="症例3" loading="lazy">
+                                <img src="../images/clinics/dio/dio_case/dio_case03.jpg" alt="症例3" loading="lazy">
                             </div>
                         </div>
                     </div>
