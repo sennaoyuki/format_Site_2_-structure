d0a5ba8fac36725062bbf99732548ba1b55f960b
senjinpc-25 <hattaryoga@Macbook-Air.local>
2025-07-31 10:06:05 +0900

    CASEスライダーをシンプルな構造に変更
    - HTMLを参考コードと同じシンプルな構造に変更
    - CSSを最小限に削減
    - JavaScriptもdots: trueのみの最小設定に変更
    - クラス名をcase-sliderに統一

--- public/draft/app.js
+++ public/draft/app.js
@@ -2245,13 +2245,9 @@ class RankingApp {
                 <div class="clinic-points-section">
                     <h4 class="section-title">CASE</h4>
                     <div class="ribbon_point_box_no">
-                        <div class="case-slick-slider" data-slider-id="${rank}">
-                            <div class="case-slide">
-                                <img src="../images/clinics/dio/dio_case/dio_case01.jpg" alt="症例1" loading="lazy">
-                            </div>
-                            <div class="case-slide">
-                                <img src="../images/clinics/dio/dio_case/dio_case02.jpg" alt="症例2" loading="lazy">
-                            </div>
+                        <div class="case-slider">
+                            <div><img src="../images/clinics/dio/dio_case/dio_case01.jpg" alt="症例1"></div>
+                            <div><img src="../images/clinics/dio/dio_case/dio_case02.jpg" alt="症例2"></div>
                         </div>
                     </div>
                 </div>
