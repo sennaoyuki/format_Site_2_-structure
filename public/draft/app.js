6c9482f9a2477ba11b9653f835ca704348ab6ccd
senjinpc-25 <hattaryoga@Macbook-Air.local>
2025-07-31 10:20:51 +0900

    動作していた単一画像表示の構成に戻す
    - case-imageクラスでラップ
    - 画像パスを../images/に戻す
    - CSSも対応するスタイルに更新

--- public/draft/app.js
+++ public/draft/app.js
@@ -2245,7 +2245,9 @@ class RankingApp {
                 <div class="clinic-points-section">
                     <h4 class="section-title">CASE</h4>
                     <div class="ribbon_point_box_no">
-                        <img src="images/clinics/dio/dio_case/dio_case01.jpg" alt="症例1" style="width: 100%; display: block;">
+                        <div class="case-image">
+                            <img src="../images/clinics/dio/dio_case/dio_case01.jpg" alt="症例1" loading="lazy">
+                        </div>
                     </div>
                 </div>
                 ` : ''}
